f148810a1a8600f4e5a364df2806bbc8decff48b
vanyukovakristina <vanyukovakristina01@mail.ru>
2022-09-19 12:05:19 +0300

    Сделаны стрелочки, выбор языка, 2 блока из main, отсортировала стили 19.09.2022

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const swiper = new Swiper('.swiper', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-prev',
-        prevEl: '.swiper-button-next',
+        nextEl: '.slider__btn-prev',
+        prevEl: '.slider__btn-next',
     },
 
     // And if we need scrollbar
@@ -23,7 +23,6 @@ const popup = document.querySelector("#popup");
 const body = document.body;
 // Клонируем меню, чтобы задать свои стили для мобильной версии
 const menu = document.querySelector("#menu").cloneNode(1);
-
 // При клике на иконку hamb вызываем ф-ию hambHandler
 hamb.addEventListener("click", hambHandler);
 
@@ -40,4 +39,34 @@ function hambHandler(e) {
 // Здесь мы рендерим элементы в наш попап
 function renderPopup() {
     popup.appendChild(menu);
+
 }
+
+/*Смена языка*/
+
+const langCurrent = document.querySelector('.languages__item--active');
+const languages = document.querySelectorAll('.languages__item');
+
+// Попробуйте её самостоятельно разобрать
+const siblings = el => [].slice.call(el.parentNode.children).filter(child => (child !== el)); // объявляем функцию, которая будет возвращать соседние элементы
+
+languages.forEach(lang => {
+    lang.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        if (lang.classList.contains('languages__item--active')) {
+            siblings(lang).forEach(el => { // для каждого соседнего элемента
+                el.classList.add('languages__item--show') // добавляем класс для показа
+            })
+        } else {
+            lang.classList.add('languages__item--active'); // добавляем класс активности
+            lang.classList.remove('languages__item--show'); // удаляем класс для показа
+
+            siblings(lang).forEach(el => {
+                el.classList.remove('languages__item--active');
+                el.classList.remove('languages__item--show');
+            })
+        }
+
+    });
+
+});
